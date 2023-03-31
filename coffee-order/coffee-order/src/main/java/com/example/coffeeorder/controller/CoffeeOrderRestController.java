a8f4bb6ebe7c1c49f1d551d95cd6dcb47d8116d6
hyegyeong-kang <rkdgprud96@gmail.com>
2023-03-31 11:24:14 +0900

    add RequestMapping annotation for fix url

--- coffee-order/coffee-order/src/main/java/com/example/coffeeorder/controller/CoffeeOrderRestController.java
+++ coffee-order/coffee-order/src/main/java/com/example/coffeeorder/controller/CoffeeOrderRestController.java
@@ -1,13 +1,15 @@
 package com.example.coffeeorder.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/coffee-order")
 public class CoffeeOrderRestController {
 
-    @GetMapping("/order")
-    public String order() {
-        return "coffee order";
+    @GetMapping("/")
+    public String coffeeOrder() {
+        return "coffee-order Serivce";
     }
 }
--- coffee-status/coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
+++ coffee-status/coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
@@ -2,13 +2,15 @@ package com.example.coffeestatus.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/coffee-status")
 public class CoffeeStatusRestController {
 
-    @GetMapping("/status")
-    public String status() {
-        return "coffee status";
+    @GetMapping("/")
+    public String coffeeStatus() {
+        return "coffee-status Service";
     }
 }
